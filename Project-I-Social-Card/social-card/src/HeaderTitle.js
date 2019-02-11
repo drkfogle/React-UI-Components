def657c880d15ac372b6ae01be7f875f6088b573
drkfogle <rodriguez.kevin.094@gmail.com>
2019-02-11 18:50:00 -0500

    sttuffs

--- Project-I-Social-Card/social-card/src/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/HeaderTitle.js
@@ -7,6 +7,7 @@ function HeaderTitle() {
         <div className="header-title">
         <h1>Lambda School</h1>
         <h2>@LambdaSchool</h2>
+        <h2>.</h2>
         <h2>26 Jan</h2>
         </div>
     );
