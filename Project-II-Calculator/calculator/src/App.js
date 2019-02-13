0367b2ffc70fe1d40de2dc8c3b93014bffbd32ed
drkfogle <rodriguez.kevin.094@gmail.com>
2019-02-12 19:33:12 -0500

    calc_style

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import './App.css';
-import CalcDisplay from './components/DisplayComponents/CalculatorDisplay';
-
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
 
 
 
 const App = () => {
   return (
-    <div>
-      <CalcDisplay />
+    <div className='app'>
+    <CalculatorDisplay text='0'/>
+      <div className='row-1'>
+      <ActionButton buttonStyle='clear' action='Clear'/>
+      <ActionButton buttonStyle='action' action='/'/>
+      </div>
+      <div className='row-2'>
+      <NumberButton buttonStyle='number' text='1'/>
+      <NumberButton buttonStyle='number' text='2'/>
+      <NumberButton buttonStyle='number' text='3'/>
+      <ActionButton buttonStyle='action' action='X'/>
+      </div>
+      <div className='row-3'>
+      <NumberButton buttonStyle='number' text='4'/>
+      <NumberButton buttonStyle='number' text='5'/>
+      <NumberButton buttonStyle='number' text='6'/>
+      <ActionButton buttonStyle='number' buttonStyle='action' action='-'/>
+      </div>
+      <div className='row-4'>
+      <NumberButton buttonStyle='number' text='7'/>
+      <NumberButton buttonStyle='number' text='8'/>
+      <NumberButton buttonStyle='number' text='9'/>
+      <ActionButton buttonStyle='action' action='+' />
+      <div className='row-5'>
+      <ActionButton buttonStyle='clear' action='0'/>
+      <ActionButton buttonStyle='action' action='=' />
+      </div>
+    </div>
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -2,38 +2,13 @@ import React from 'react';
 import './Display.css';
 
 
-const CalcDisplay = props => {
+const CalculatorDisplay = props => {
     return (
         <div>
-            <div className='row-1'>
-            <button>Clear</button>
-            <button><i class="fas fa-divide"></i></button>
-            </div>
-            <div className='row-2'>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>X</button>
-            </div>
-            <div className='row-3'>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>-</button>
-            </div>
-            <div className='row-4'>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>+</button>
-            </div>
-            <div className='row-5'>
-            <button>0</button>
-            <button>=</button>
-            </div>
+        <h1 className='display'>{props.text}</h1>
         </div>
     );
 }
 
 
-export default CalcDisplay;
+export default CalculatorDisplay;
